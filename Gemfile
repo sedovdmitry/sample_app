c36efdeefdc30c698a8aaa8c2ba82b74221d8b88
Dmitry Sedov <sedov.dmitry1@gmail.com>
2015-02-27 23:26:30 +0300

    Add spork-testing

--- Gemfile
+++ Gemfile
@@ -6,11 +6,16 @@ gem 'rails', '4.2.0'
 group :development, :test do
   gem 'sqlite3', '1.3.10'
   gem 'rspec-rails', '3.2.1'
+  gem 'guard-rspec', '4.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '2.1.0'
+  gem 'childprocess', '0.5.5'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.44.0'
   gem 'capybara', '2.4.4'
+  gem 'libnotify', '0.9.1'
 end
 
 gem 'sass-rails', '5.0.1'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,39 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'spork'
 
-require 'capybara/rspec'
-Capybara.javascript_driver = :webkit
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-RSpec.configure do |config|
-# ## Mock Framework
-#
-# If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-#
-# config.mock_with :mocha
-# config.mock_with :flexmock
-# config.mock_with :rr
-# Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-#config.fixture_path = "#{::Rails.root}/spec/fixtures"
-# If you're not using ActiveRecord, or you'd prefer not to run each of your
-# examples within a transaction, remove the following line or assign false
-# instead of true.
-#config.use_transactional_fixtures = true
-# If true, the base class of anonymous controllers will be inferred
-# automatically. This will be the default behavior in future versions of
-# rspec-rails.
-config.infer_base_class_for_anonymous_controllers = false
-config.include Capybara::DSL
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  
+  RSpec.configure do |config|
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
+
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, comment the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
+  end
+end
+
+Spork.each_run do
 end
